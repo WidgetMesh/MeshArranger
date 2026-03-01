887bc9273840d439edb28758b601ca0865d79458
John Gentilin <gentijo@gmail.com>
2026-02-28 16:27:48 -0800

    update

--- dnet/__init__.py
+++ dnet/__init__.py
@@ -1,6 +1,6 @@
 import gc
 gc.enable()
-print("Loading LighthouseMesg")
+print("Loading LighthouseMesh")
 from dnet.signalling.LighthouseMesh import LighthouseMesh
 print("loading Payload")
 from dnet.signalling.Payload import Payload
